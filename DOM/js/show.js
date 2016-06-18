0d7fadde8e1b17d827328963e198f35eff28af23
austincunningham <austincunningham@hotmail.com>
2016-06-18 11:29:52 +0100

    javascript 3-5 style.visibility js vs jquery

---
+++ DOM/js/show.js
@@ -0,0 +1,18 @@
+/**
+ * Created by austin on 18/06/2016.
+ */
+//js style.visibility
+/*function hide()  {
+  document.getElementById('text').style.visibility = 'hidden';
+};
+function reveal()  {
+  document.getElementById('text').style.visibility = 'visible';
+};*/
+//jQuery style visibility
+function hide() {
+  $('#text').hide();
+}
+
+function reveal() {
+  $('#text').show();
+}
