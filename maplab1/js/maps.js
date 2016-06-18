5c1a60c12506ad1cc93138fd0d42152ce2c3bd4b
austincunningham <austincunningham@hotmail.com>
2016-06-18 21:59:08 +0100

    map lab 1 set a marker

--- maplab1/js/maps.js
+++ maplab1/js/maps.js
@@ -3,12 +3,25 @@
  */
 function initialize()
 {
+  const latlng = new google.maps.LatLng(52.254427, -7.185281);
   const mapOptions =
   {
     center: new google.maps.LatLng(52.254427, -7.185281),
-    zoom: 12,
+    mapTypeId: google.maps.MapTypeId.SATELLITE,
+    zoom: 16,
   };
   const map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
+
+  // place a marker
+  const marker = new google.maps.Marker({
+    map: map,
+    draggable: true,
+    position: latlng,
+    title: 'Drag and drop on your property!',
+  });
+
+  // To add the marker to the map, call setMap();
+  marker.setMap(map);
 };
 google.maps.event.addDomListener(window, 'load', initialize);
