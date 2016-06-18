abfdd8a99b78dd0660e2a6a325c9b6dfed4b7c9e
austincunningham <austincunningham@hotmail.com>
2016-06-18 23:04:41 +0100

    map lab 1 add listner to update as marker moved

--- maplab1/js/maps.js
+++ maplab1/js/maps.js
@@ -4,24 +4,36 @@
 function initialize()
 {
   const latlng = new google.maps.LatLng(52.254427, -7.185281);
+
   const mapOptions =
   {
-    center: new google.maps.LatLng(52.254427, -7.185281),
+    center: new google.maps.LatLng(latlng.lat(), latlng.lng()),
     mapTypeId: google.maps.MapTypeId.SATELLITE,
     zoom: 16,
   };
-  const map = new google.maps.Map(document.getElementById('map-canvas'),
+  const map = new google.maps.Map(document.getElementById("map-canvas"),
       mapOptions);
 
   // place a marker
   const marker = new google.maps.Marker({
     map: map,
-    draggable: true,
     position: latlng,
     title: 'Drag and drop on your property!',
+    draggable: true,
   });
 
-  // To add the marker to the map, call setMap();
+  // This adds the marker to the map
   marker.setMap(map);
-};
+
+  //marker listener populates hidden fields ondragend
+  google.maps.event.addListener(marker, 'dragend', function () {
+    const latLng = marker.getPosition();
+    const latlong = latLng.lat().toString().substring(0, 10) + ',' + latLng.lng().toString().substring(0, 10);
+    //publish lat long in geolocation control in html page
+    $('#geolocation').val(latlong);
+    //update the new marker position
+    map.setCenter(latLng);
+  });
+
+}
 google.maps.event.addDomListener(window, 'load', initialize);
