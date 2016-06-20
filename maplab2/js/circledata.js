a2943272a19dbae520ac016d1f8d7b24d4123342
austincunningham <austincunningham@hotmail.com>
2016-06-20 23:36:17 +0100

    Circle-map-overlay 04

--- maplab2/js/circledata.js
+++ maplab2/js/circledata.js
@@ -3,15 +3,16 @@
  */
 let circle;
 let map;
+let marker;
 
 function circleData() {
   const center = circle.getCenter();
   const latcenter = center.lat().toString();
   const lngcenter = center.lng().toString();
   const radius = circle.getRadius().toString();
-  $("#radius").val(radius);
-  $("#latcenter").val(latcenter);
-  $("#lngcenter").val(lngcenter);
+  $('#radius').val(radius);
+  $('#latcenter').val(latcenter);
+  $('#lngcenter').val(lngcenter);
 }
 
 function initialize()
@@ -41,6 +42,25 @@ function initialize()
   //Enable to allow user to vary radius by dragging anchor point
   circle.setEditable(true);
   circle.setMap(map);
+  addMarker(map, center);
+};
+function addMarker(map, center)
+{
+  // create a marker
+  marker = new google.maps.Marker({
+    map: map,
+    position: center,
+    title: 'Drag and drop on your property!',
+    draggable: true,
+  });
+  marker.setMap(map);
 };
 
+function markerData()
+{
+  const latLng = marker.getPosition();
+  const latlong = latLng.lat().toString().substring(0,10) + ',' + latLng.lng().toString().substring(0,10);
+  //publish lat long in geolocation control in html page
+  $('#geolocation').val(latlong);
+}
 google.maps.event.addDomListener(window, 'load', initialize);
